04e78fe2dc8a07b045ac79041bfee23e6664da7e
Quentin De Smedt <quentin.de.smedt@octo.com>
2020-07-20 17:18:39 +0200

    feat: add V6

--- assets/main.js
+++ assets/main.js
@@ -22,3 +22,13 @@ function shuffleArray(array) {
 }
 
 shuffle();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
